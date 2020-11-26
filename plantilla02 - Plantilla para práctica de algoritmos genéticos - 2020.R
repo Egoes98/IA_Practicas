2c41d654eca4efdc13e553ee9b6634d413f728bc
Egoitz <egoes98@gmail.com>
2020-11-26 01:20:03 +0100

    Terminado codigo
    Algoritmo implementado. Falta documentar y hacer las pruebas que se piden

--- plantilla02 - Plantilla para práctica de algoritmos genéticos - 2020.R
+++ plantilla02 - Plantilla para práctica de algoritmos genéticos - 2020.R
@@ -44,47 +44,41 @@ for(i in 1:long){
 fitness = function(ind,distancias){
   ruta1 = 0
   ruta2 = 0
-  
-  zero = FALSE
-  ruta2Existe = FALSE
-  
-  primeroR1 = ind[1]
-  primeroR2 = 0
+  rutaTotal = 0
 
-  if(length(ind)==1 || ind[2] == 0 || ind[length(ind)-1] == 0){
+  zeroPos = match(0,ind)
+  
+  #En caso de que una ruta tenga solo un punto, o no tenga ninguno devuelve Inf ya qye he decidido que quiero evitar que uno de los caminos no tenga una ruta.
+  #Al devolver infinito quiero que se pase por alto estos casos concretos
+  if(length(ind) == 1 || zeroPos == 2 || zeroPos == (length(ind)-1) || zeroPos == 1 || zeroPos == length(ind)){
     return(Inf)
   }
+  
+  puntosRuta1 = ind[1:zeroPos-1]
+  
+  sizePR2 = length(ind)-length(puntosRuta1)-1
+  puntosRuta2 = ind[zeroPos+1:sizePR2]
+  
+  for(i in 2:length(puntosRuta1)){
+    ruta1 = ruta1 + distancias[puntosRuta1[i],puntosRuta1[i-1]]
+  }
+  ruta1 = ruta1 + distancias[puntosRuta1[1],puntosRuta1[length(puntosRuta1)]]
+  
+  for(i in 2:length(puntosRuta2)){
+    ruta2 = ruta2 + distancias[puntosRuta2[i],puntosRuta2[i-1]]
+  }
+  ruta2 = ruta2 + distancias[puntosRuta2[1],puntosRuta2[length(puntosRuta2)]]
+  
+  rutaTotal = ruta1[1] + ruta2[1]
+  
+  #He decido que voy a premiar que las rutas tengas, mas o menos los mismos puntos
+  puntoCentral = as.integer(length(ind)/2)
+  if(zeroPos == puntoCentral || zeroPos == (puntoCentral+1)){
+    rutaTotal = rutaTotal - 2000
+  }
 
-  for(i in 2:length(ind)){
-    if(zero){
-      if(ind[i-1] != 0){
-        ruta2Existe = true
-        ruta2 = ruta2 + distancias[ind[i],ind[i-1]]
-      }else{
-        primeroR2 = ind[i]
-      }
-    }else{
-      if(ind[i] == 0){
-        ruta1 = ruta1 + distancias[primeroR1,ind[i-1]]
-        zero = TRUE
-      }else{
-        ruta1 = ruta1 + distancias[ind[i],ind[i-1]]
-      }
-    }
-  }
-  
-  if(ruta2Existe){
-    ruta2 = ruta2 + distancias[primeroR2,ind[length(ind)]]
-  }
-  
-  ruta1+ruta2
+  return(rutaTotal)
 }
-
-#TODO BORRAR ESTO SOLO PARA PRUEBAS
-individuo = c(21,1,0,3)
-distancia = fitness(individuo,distancias)
-distancia
-
 
 # Paso 3: Defina la función de inicialización, que reciba el número de individuos a crear
 # y el número de puntos (variable long). Ésta función debe devolver una matriz con tantas
@@ -94,12 +88,10 @@ distancia
 initial = function(number, long){
   individuos = matrix(nrow = number, ncol = long+1)
   for(i in 1:number){
-    individuos [i,] = append(sample(1:29,29,replace = TRUE),0,after = sample(1:29,1))
+    individuos [i,] = append(sample(1:29,29,replace = FALSE),0,after = sample(1:29,1))
   }
   individuos
 }
-
-matriz = initial(4,long)
 
 # Paso 4: Puede utilizar la función de torneo binario vista en clase
 tournamentselection = function(evaluation,number){
@@ -126,9 +118,46 @@ tournamentselection = function(evaluation,number){
 # -->     return(offspring)
 # -->   }
 
+crossover = function(indexparents, population,pcross){
+  offspring = population
+  for(i in seq(1, length(indexparents),2)){
+    if(runif(1)<pcross){
+      cutPoint = sample(1:ncol(population),2,replace = FALSE)
+      cutPoint = sort(cutPoint,decreasing = FALSE)
+      
+      cutValues1 = population[i,cutPoint[1]:cutPoint[2]]
+      cutValues2 = population[i+1,cutPoint[1]:cutPoint[2]]
+      
+      newOffspring1 = setdiff(population[i+1,],cutValues1)
+      newOffspring2 = setdiff(population[i,],cutValues2)
+      
+      newOffspring1 = append(newOffspring1, cutValues1, after = cutPoint[1])
+      newOffspring2 = append(newOffspring2, cutValues2, after = cutPoint[1])
+      
+      offspring[i,] = newOffspring1
+      offspring[i+1,] = newOffspring2 
+    }
+  }
+  return(offspring)
+}
+
+#cada 2 se ejecuta coges el index parent i y i+1 dices que dos cortar con sample, de cada uno coges y guardas los que vas a quitar. Quitarle a cada uno lo del otro
+
+
 # Paso 6: Operador de Mutación
 # Para cada hijo, con probabilidad pmut, intercambiar dos posiciones elegidas aleatoriamente
 # -->   mutation = function(population,pmut){...}
+mutation = function(population,pmut){
+    for(i in 1:nrow(population)){
+      if(runif(1)<pmut){
+        pos = sample(1:ncol(population),2,replace = FALSE)
+        value = population[i,pos[1]]
+        population[i,pos[1]] = population[i,pos[2]]
+        population[i,pos[2]] = value
+      }
+    }
+  population
+}
 
 # Paso 7: Realize hasta 5 pruebas con diferentes configuraciones de los siguientes parámetros
 # analice y comente los resultados a modo de comentarios:
@@ -138,7 +167,7 @@ tournamentselection = function(evaluation,number){
 # Resultados--> ...
 # Justificación/Razonamiento--> ...
 #---------------------------------------------------------------------------
-generations = 500
+generations = 1000
 tournamentsize = 2
 probcrossover = 0.9
 probmutation  = 0.05
@@ -203,3 +232,4 @@ plot2 = ggplot(progreso,aes(x=g,y=distancia))+geom_line()+
        subtitle = "Ojo, en este caso la distancia euclídea no es significativa",
        caption = "Universidad de deusto")
 grid.arrange(plot1,plot2,ncol=1)
+
